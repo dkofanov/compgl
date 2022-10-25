89388a8750de5683c4befe8f4262c441dc12edfd
Daniel Kofanov <kofanov.daniil@huawei-partners.com>
2022-10-26 01:18:31 +0300

    Refactor llvm p1

--- main_llvm/main_llvm.cpp
+++ main_llvm/main_llvm.cpp
@@ -10,64 +10,37 @@
 #include "llvm/Support/TargetSelect.h"
 #include <iostream>
 
-    #define VOID_T builder_.getVoidTy()
-    #define PTR_T builder_.getInt8PtrTy()
-    #define I8_T builder_.getInt8Ty()
-    #define I32_T builder_.getInt32Ty()
-    #define I64_T builder_.getInt64Ty()
-void test()
-{
-}
+
 
 class IRGen {
 public:
+#define __ builder_. 
+#define VOID_T __ getVoidTy()
+#define PTR_T __ getInt8PtrTy()
+#define I8_T __ getInt8Ty()
+#define I32_T __ getInt32Ty()
+#define I64_T __ getInt64Ty()
+
+
     IRGen() : context_(), builder_(context_), module_(new llvm::Module("top", context_))
     {
         llvm::InitializeNativeTarget();
         llvm::InitializeNativeTargetAsmPrinter();
     }
 
-    void CreateMain() {
-
+    void DeclareGlobals()
+    {
         // declare ENG struct:
         llvm::StructType *eng_ty = llvm::StructType::create(context_, GetArgsType({PTR_T, PTR_T, I64_T, I64_T}));
-        module_->getOrInsertGlobal("ENG_DATA", eng_ty);
-        llvm::GlobalVariable *eng = module_->getNamedGlobal("ENG_DATA");
-        eng->setLinkage(llvm::GlobalVariable::ExternalLinkage);
+        static_cast<llvm::GlobalVariable *>(module_->getOrInsertGlobal("ENG_DATA", eng_ty))->setLinkage(llvm::GlobalVariable::ExternalLinkage);;
+    }
 
+    void DeclareFunctions()
+    {
         // declare void @main()
         main_ = GetFunction(I32_T, GetArgsType({}), "main");
-
-        // entrypoint:
-        auto *entry = llvm::BasicBlock::Create(context_, "entrypoint", main_);
-        builder_.SetInsertPoint(entry);
-
-        auto wrld_buf = builder_.CreateAlloca(I8_T, I64(25000));
-        auto wrld_mirror_buf = builder_.CreateAlloca(I8_T, I64(25000));
-
-        auto gl_init = GetFunction(VOID_T, GetArgsType({I64_T, I64_T, I64_T}), "GR_Initialize");
-        builder_.CreateCall(gl_init, GetArgs({I64(1250), I64(500), I64(5)}));
-
-        builder_.CreateLifetimeStart(wrld_buf, I64(25000));
-        builder_.CreateMemSet(wrld_buf, I8(0), I64(25000), llvm::MaybeAlign(16));
-        builder_.CreateLifetimeStart(wrld_mirror_buf, I64(25000));
-        builder_.CreateMemSet(wrld_mirror_buf, I8(0), I64(25000), llvm::MaybeAlign(16));
-        builder_.CreateStore(wrld_buf, eng);
-        auto idx1 = GetArgs({I64(0), I32(1)});
-        auto idx2 = GetArgs({I64(0), I32(2)});
-        auto idx3 = GetArgs({I64(0), I32(3)});
-        auto eng_2 = builder_.CreateGEP(eng, idx1);
-        auto eng_3 = builder_.CreateGEP(eng, idx2);
-        auto eng_4 = builder_.CreateGEP(eng, idx3);
-        builder_.CreateStore(wrld_mirror_buf, eng_2);
-        builder_.CreateStore(I64(250), eng_3);
-        builder_.CreateStore(I64(100), eng_4);
-
+        GetFunction(VOID_T, GetArgsType({I64_T, I64_T, I64_T}), "GR_Initialize");
         auto eng_fill_rand = GetFunction(VOID_T, {I32_T}, "ENG_FillRandom");
-        builder_.CreateCall(eng_fill_rand, GetArgs({I32(222)}));
-
-        llvm::BasicBlock *loop = llvm::BasicBlock::Create(context_, "loop", main_);
-        builder_.CreateBr(loop);
 
         auto callb_ty = GetFunctionType(VOID_T, GetArgsType({PTR_T, I64_T, I64_T}));
         auto callb_draw = GetFunction(callb_ty, "CallbackDraw");
@@ -77,16 +50,71 @@ public:
         auto swap = GetFunction(VOID_T, GetArgsType({}), "ENG_SwapBuffers");
         auto gl_flush = GetFunction(VOID_T, GetArgsType({}), "GR_Flush");
         auto sleep = GetFunction(I32_T, GetArgsType({I32_T}), "usleep");
-        builder_.SetInsertPoint(loop);
-            // builder_.CreatePHI(I64_T, 2);
-            builder_.CreateCall(iter_wrld, callb_calc);
-            builder_.CreateCall(iter_wrld, callb_draw);
-            builder_.CreateCall(swap);
-            builder_.CreateCall(gl_flush);
-            builder_.CreateCall(sleep, GetArgs({I32(1)}));
-            builder_.CreateBr(loop);
 
-        builder_.CreateRet(llvm::ConstantInt::get(I32_T, 0, true));
+    }
+
+    void CreateIR()
+    {
+        DeclareGlobals();
+        DeclareFunctions();
+
+        CreateMain();
+        //CreateIterateWorld();
+    }
+
+    void CreateIterateWorld()
+    {
+        // entrypoint:
+        auto *entry = llvm::BasicBlock::Create(context_, "entrypoint", Func("ENG_IterateWorld"));
+        __ SetInsertPoint(entry);
+        llvm::GlobalVariable *eng = module_->getNamedGlobal("ENG_DATA");
+        auto gep_2 = __ CreateGEP(eng, GetArgs({I64(0), I32(2)}));
+        auto gep_3 = __ CreateGEP(eng, GetArgs({I64(0), I32(3)}));
+        auto eng_2 = __ CreateLoad(gep_2);
+        auto eng_3 = __ CreateLoad(gep_3);
+        auto mul = __ CreateMul(eng_2, eng_3);
+        auto 
+    }
+
+    void CreateMain()
+    {
+        llvm::GlobalVariable *eng = module_->getNamedGlobal("ENG_DATA");
+
+        // entrypoint:
+        auto *entry = llvm::BasicBlock::Create(context_, "entrypoint", main_);
+        __ SetInsertPoint(entry);
+
+        auto wrld_buf = __ CreateAlloca(I8_T, I64(25000));
+        auto wrld_mirror_buf = __ CreateAlloca(I8_T, I64(25000));
+
+        __ CreateCall(Func("GR_Initialize"), GetArgs({I64(1250), I64(500), I64(5)}));
+
+        __ CreateLifetimeStart(wrld_buf, I64(25000));
+        __ CreateMemSet(wrld_buf, I8(0), I64(25000), llvm::MaybeAlign(16));
+        __ CreateLifetimeStart(wrld_mirror_buf, I64(25000));
+        __ CreateMemSet(wrld_mirror_buf, I8(0), I64(25000), llvm::MaybeAlign(16));
+        __ CreateStore(wrld_buf, eng);
+        auto eng_1 = __ CreateGEP(eng, GetArgs({I64(0), I32(1)}));
+        auto eng_2 = __ CreateGEP(eng, GetArgs({I64(0), I32(2)}));
+        auto eng_3 = __ CreateGEP(eng, GetArgs({I64(0), I32(3)}));
+        __ CreateStore(wrld_mirror_buf, eng_1);
+        __ CreateStore(I64(250), eng_2);
+        __ CreateStore(I64(100), eng_3);
+
+        __ CreateCall(Func("ENG_FillRandom"), I32(222));
+
+        llvm::BasicBlock *loop = llvm::BasicBlock::Create(context_, "loop", main_);
+        __ CreateBr(loop);
+
+        __ SetInsertPoint(loop);
+            __ CreateCall(Func("ENG_IterateWorld"), Func("CallbackCalc"));
+            __ CreateCall(Func("ENG_IterateWorld"), Func("CallbackDraw"));
+            __ CreateCall(Func("ENG_SwapBuffers"));
+            __ CreateCall(Func("GR_Flush"));
+            __ CreateCall(Func("usleep"), I32(4000));
+            __ CreateBr(loop);
+
+        __ CreateRet(llvm::ConstantInt::get(I32_T, 0, true));
         module_->print(llvm::outs(), nullptr);
     }
 
@@ -94,14 +122,16 @@ public:
         llvm::ExecutionEngine *ee = llvm::EngineBuilder(std::unique_ptr<llvm::Module>(module_)).create();
         ee->InstallLazyFunctionCreator([&](const std::string &fn) -> void *
             {
-                if (fn == "ENG_FillRandom") { return reinterpret_cast<void *>(ENG_FillRandom); }
+                if (fn == "usleep") { return reinterpret_cast<void *>(usleep); }
+                if (fn == "GR_Flush") { return reinterpret_cast<void *>(GR_Flush); }
                 if (fn == "GR_Initialize") { return reinterpret_cast<void *>(GR_Initialize); }
+
+                if (fn == "ENG_IterateWorld") { return reinterpret_cast<void *>(ENG_IterateWorld); }
+                if (fn == "ENG_FillRandom") { return reinterpret_cast<void *>(ENG_FillRandom); }
                 if (fn == "CallbackCalc") { return reinterpret_cast<void *>(CallbackCalc); }
                 if (fn == "CallbackDraw") { return reinterpret_cast<void *>(CallbackDraw); }
                 if (fn == "ENG_SwapBuffers") { return reinterpret_cast<void *>(ENG_SwapBuffers); }
-                if (fn == "ENG_IterateWorld") { return reinterpret_cast<void *>(ENG_IterateWorld); }
-                if (fn == "GR_Flush") { return reinterpret_cast<void *>(GR_Flush); }
-                if (fn == "usleep") { return reinterpret_cast<void *>(usleep); }
+
                 std::cerr << "NO_FUNC " << fn << std::endl;
                 return nullptr;
             });
@@ -135,16 +165,21 @@ private:
     }
     llvm::ConstantInt *I8(int8_t val)
     {
-        return llvm::ConstantInt::get(builder_.getInt8Ty(), val, true);
+        return llvm::ConstantInt::get(__ getInt8Ty(), val, true);
     }
     llvm::ConstantInt *I32(int32_t val)
     {
-        return llvm::ConstantInt::get(builder_.getInt32Ty(), val, true);
+        return llvm::ConstantInt::get(__ getInt32Ty(), val, true);
     }
     llvm::ConstantInt *I64(int64_t val)
     {
-        return llvm::ConstantInt::get(builder_.getInt64Ty(), val, true);
+        return llvm::ConstantInt::get(__ getInt64Ty(), val, true);
     }
+    llvm::Function *Func(const std::string &fn) {
+        return module_->getFunction(fn);
+    }
+#undef __
+
 private:
     // LLVM:
     llvm::LLVMContext context_;
@@ -163,7 +198,7 @@ private:
 int main()
 {
     IRGen irg;
-    irg.CreateMain();
+    irg.CreateIR();
     irg.LaunchEE();
 
     return 0;
